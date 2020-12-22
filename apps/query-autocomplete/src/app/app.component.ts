a8a09fb4cfaf8a954504127c05ac771fee171094
mbstuart <mattburnstu@gmail.com>
2020-12-22 14:07:59 +0000

    initial commit

---
+++ apps/query-autocomplete/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'query-autocomplete-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'query-autocomplete';
+}
